ca2dee8e49d40a27b0629f142c575ffbceed85bc
parveen kumar <parveen@csgroupchd.com>
2024-09-03 18:18:57 +0530

    dsa2

--- dsa_array.php
+++ dsa_array.php
@@ -1,15 +1,24 @@
 <?php
+
+//Bubble Sort
+
+
 //Selection sort
 //answer => 5,11,12,22,25,34,64,90
 $elementContainer = [64, 34, 25, 5, 22, 11, 90, 12];
 for($i=0;$i<count($elementContainer);$i++){
-    for($j=0;$j<count($elementContainer);$j++){
-        $swap1='';
-        $swap2=''; 
-        if($elementContainer[$j] < $elementContainer[$i]){
-            $swap1 = $elementContainer[$j];
-            $swap2 = $elementContainer[$i];
-        }
+    $low = $i;
+    for($j=0+$i;$j<count($elementContainer);$j++){
+       if($elementContainer[$j]<$elementContainer[$low])
+       {
+        $low = $j;
+       }
     }  
+    if ($elementContainer[$i] > $elementContainer[$low]) { 
+        $tmp = $elementContainer[$i]; 
+        $elementContainer[$i] = $elementContainer[$low]; 
+        $elementContainer[$low] = $tmp; 
+    } 
 }
+print_r($elementContainer);
 ?>
